01fb687670f5fd657bb266337e80983941f1b884
jkghdfjgdfhfdhg <kurbanov.rustam.mansurovich@gmail.com>
2023-07-08 16:56:45 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
-function App() {
+const RegistrationForm = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [showData, setShowData] = useState(false);
+  const [massege, setMassege] = useState('')
+  const onButton = () => {
+    setMassege('Success!')
+  }
+
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setShowData(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <h2>Registration Form</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="First Name"
+          value={firstName}
+          onChange={handleFirstNameChange}
+          style={{ display: 'flex',justifyContent:'center', padding: '5px', width: '200px', textAlign: 'center',alignItems:'center',flexDirection: 'column' }}
+        />
+        <input
+          type="text"
+          placeholder="Last Name"
+          value={lastName}
+          onChange={handleLastNameChange}
+          style={{display: 'flex',justifyContent:'center', padding: '5px', width: '200px', textAlign: 'center',alignItems:'center',flexDirection: 'column' }}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={handleEmailChange}
+          style={{ display: 'flex',justifyContent:'center', padding: '5px', width: '200px', textAlign: 'center',alignItems:'center',flexDirection: 'column' }}
+        />
+        <button onClick={onButton} className='button' type="submit" style={{display: 'flex',justifyContent:'center', padding: '5px', backgroundColor: 'green', color: 'white', border: 'none'}}>
+          Register
+        </button>
+        <p>{massege}</p>
+      </form>
+      {showData && (
+        <div style={{ marginTop: '10px', textAlign: 'center' }}>
+          <p>First Name: {firstName}</p>
+          <p>Last Name: {lastName}</p>
+          <p>Email: {email}</p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default RegistrationForm;
